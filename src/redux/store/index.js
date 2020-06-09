e855f23ce760f71312ef45c1059a6cb2c1779c4a
Cardoso <lsc.cardosoo@gmail.com>
2020-06-09 15:31:08 -0300

    Corrigir codeclimate do commit anterior

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,8 +2,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
-
