a6ec43fd9b5e9d434f237aa02ab1ca321569fead
DevT <aguiarthiag@gmail.com>
2020-06-10 16:27:35 -0300

    Ranking

--- src/pages/Ranking/Ranking.js
+++ src/pages/Ranking/Ranking.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends React.Component {
+  renderRanking(array) {
+    array.sort((a, b) => b.score - a.score)
+    return(
+      array.map((e, index) => <div key={e.name}>
+        <img src={e.picture} alt="avatar" />
+        <p data-testid={`player-name-${index}`}>{e.name}</p>
+        <span data-testid={`player-score-${index}`}>{e.score}</span>
+      </div>)
+    )
+  }
+
   render() {
+    const mock = [
+      {name: 'Thiago Aguiar', score: 10, picture: 'https://www.gravatar.com/avatar/1ac1132092083b6b253f323a65ff80dc'},
+      {name: 'Lucas Soares', score: 32, picture: 'https://www.gravatar.com/avatar/80651d42c055aa99d151ab6f59649982'},
+      {name: 'Clayton Pereira', score: 25, picture: 'https://www.gravatar.com/avatar/2d3bf5b67282f5f466e503d7022abcf3'},
+    ]
     return (
       <div>
-        Ranking
+        <Link to="/">
+          <button data-testid="btn-go-home">Tela inicial</button>
+        </Link>
+          {this.renderRanking(mock)}
       </div>
     );
   }
