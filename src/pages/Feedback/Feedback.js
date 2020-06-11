64904e5c86e9e34d9b57cf2183bd620f7471b5a7
cpereira <cpereiramt@gmail.com>
2020-06-10 22:34:38 -0400

    Salvando alterações iniciais tela Feedback

--- src/pages/Feedback/Feedback.js
+++ src/pages/Feedback/Feedback.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
 class Feedback extends React.Component {
+
   render() {
+    const state = {
+      player: {
+        name : 'clayton',
+        assertions: 2,
+        score:200,
+        gravatarEmail:'fulano@gmail.com'
+    }
+    }
     return (
       <div>
-        Feedback
+      <img data-testid="header-profile-picture" alt="image" />
+      <h2 data-testid="header-player-name">Name</h2>
+      <h2 data-testeid="header-score-">Placar</h2>
+      {state.player.assertions < 3 ? <h1 data-testid="feedback-text"> Podia ser melhor...</h1> : <h1 data-testid="feedback-text
+">Mandou bem!</h1>}
       </div>
     );
   }
