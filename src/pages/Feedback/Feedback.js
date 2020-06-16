2b594baa7a11ccd2b1c34608a27a4e1b2e4c481f
cpereira <cpereiramt@gmail.com>
2020-06-16 18:09:27 -0400

    alterações para o codeClimate

--- src/pages/Feedback/Feedback.js
+++ src/pages/Feedback/Feedback.js
@@ -7,21 +7,20 @@ import Header from '../Game/Header/Header';
 class Feedback extends React.Component {
 
   render() {
-   const { score, assertions } = this.props;
-   let feedback;
-   console.log(assertions);
-   if(assertions < 3 ) {
-   feedback = 'Podia ser melhor...';
+    const { score, assertions } = this.props;
+    let feedback;
+    if (assertions < 3) {
+     feedback = 'Podia ser melhor...';
    } else if (assertions >= 3) {
-   feedback = 'Mandou bem!';
+     feedback = 'Mandou bem!';
    }
 
-   return (
-      <div>
+    return (
+     <div>
         <Header />
         <h3 data-testid="feedback-text">{feedback}</h3>
-        <label>Total de Pontos: <p data-testid="feedback-total-score">{score}</p></label>
-        <label>Total de Pontos:<h5 data-testid="feedback-total-question">{assertions}</h5></label>
+        <label htmlFor="score">Total de Pontos: <p name="score" data-testid="feedback-total-score">{score}</p></label>
+        <label htmlFor="assertion">Total de Pontos:<h5 name="assertion" data-testid="feedback-total-question">{assertions}</h5></label>
         <Link to="/ranking"><button data-testid="btn-ranking">Ver Ranking</button></Link>
         <Link to="/"> <button data-testid="btn-play-again">Jogar novamente</button></Link>
       </div>
@@ -34,6 +33,8 @@ Feedback.propTypes = {
     category: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
   })).isRequired,
+  score: PropTypes.string.isRequired,
+  assertions: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = (state) => ({
